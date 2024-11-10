be58936f2c1a7c411ea0398fb6873449d3dd339e
Lilith Iris <itslirissama@gmail.com>
2024-11-10 14:25:49 +0700

    test

--- pages/src/app/page.tsx
+++ pages/src/app/page.tsx
@@ -151,7 +151,6 @@ export default function Landing() {
     rssSectionRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-  const basePath = process.env.NODE_ENV === "production" ? "VBT" : "";
   return (
     <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
       {/* Floating Background */}
@@ -160,7 +159,7 @@ export default function Landing() {
         {floatingImages.map((img, index) => (
           <FloatingImage
             key={index}
-            src={`${basePath}${img}`}
+            src={`${img}`}
             position={imagePositions[index] || { left: 0, top: 0 }}
             index={index}
           />
