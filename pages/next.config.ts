9e070c936c1844728db10fbcde3c972ce4f6303e
Lilith Iris <itslirissama@gmail.com>
2024-11-10 14:10:05 +0700

    unoptimized Image and and dev basepath
    add unoptimized for images and add base path if production build

--- pages/next.config.ts
+++ pages/next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/VBT",
+  basePath: process.env.NODE_ENV === "production" ? "/VBT" : "",
   output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
